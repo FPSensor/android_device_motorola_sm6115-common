4f75f3f68e59f87d746a76aab30b2aa4747ca38b
Erfan Abdi <erfangplus@gmail.com>
2020-12-17 23:44:10 +0330

    sm6115-common: Import LineageActions from sdm660-common

--- sm6115.mk
+++ sm6115.mk
@@ -304,6 +304,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.motorola_bengal
 
+# LineageActions
+PRODUCT_PACKAGES += \
+    LineageActions
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.motorola_bengal
