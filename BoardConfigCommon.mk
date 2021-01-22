9b5bec8a32404531c2078d9ed035d8fd73468143
Erfan Abdi <erfangplus@gmail.com>
2021-01-22 17:15:37 +0330

    sm6115-common: Initial SELinux setup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,10 +206,8 @@ VENDOR_SECURITY_PATCH := 2021-01-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
